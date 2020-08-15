0c77aa288e2667021504e44b1a8bf4bbab60fc28
c00kamunga <tsothalanti2@yahoo.com>
2020-08-14 19:33:05 -0700

    error with post request, nothing printing out in insomnia

--- actions-Router.js
+++ actions-Router.js
@@ -3,6 +3,8 @@ const projectModel = require('./data/helpers/projectModel')
 const actionsModel = require('./data/helpers/actionModel')
 const router = require("express").Router();
 
+
+//end point for getting actions
 router.get('/api/actions', async (req,res) => {
     try {
         const actions = await actionsModel.get()
@@ -12,6 +14,25 @@ router.get('/api/actions', async (req,res) => {
         .status(500)
         .json({ error: 'The actions information could not be retrieved.' })
     }
+})
+
+
+//endpoint for creating an action for a project
+router.post('/api/project/:id/actions', (req, res) => {
+    if(!req.body.description && !req.body.notes){
+        res
+        .json(400)
+        .json({ errorMessage: "Please provide description and notes for the action." })
+    }
+    actionsModel.insert(req.body)
+    .then(newAction => {
+        return res
+        .status(201)
+        .json(newAction)
+    })
+    .catch(error => {
+        error: "There was an error while saving the action to the database."
+    })
 })
 
 module.exports = router
